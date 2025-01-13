ee0a652990efe938a0eb36c7ee2a80a8a0dc2c7d
ryankubanempire <ryan@kubanempire.com>
2025-01-13 13:07:21 +0800

    initial commit

--- src/Schema/Element/Any/Any.php
+++ src/Schema/Element/Any/Any.php
@@ -7,14 +7,16 @@ namespace GoetasWebservices\XML\XSDReader\Schema\Element\Any;
 use GoetasWebservices\XML\XSDReader\Schema\Element\ElementItem;
 use GoetasWebservices\XML\XSDReader\Schema\Element\InterfaceSetMinMax;
 use GoetasWebservices\XML\XSDReader\Schema\Element\MinMaxTrait;
+use GoetasWebservices\XML\XSDReader\Schema\Item;
 use GoetasWebservices\XML\XSDReader\Schema\Schema;
 use GoetasWebservices\XML\XSDReader\Schema\SchemaItem;
 use GoetasWebservices\XML\XSDReader\Schema\SchemaItemTrait;
+use GoetasWebservices\XML\XSDReader\Schema\Type\AnyType;
 
 /**
  * Represents https://www.w3schools.com/xml/el_any.asp.
  */
-class Any implements SchemaItem, ElementItem, InterfaceSetMinMax
+class Any extends Item implements SchemaItem, ElementItem, InterfaceSetMinMax
 {
     use MinMaxTrait;
     use SchemaItemTrait;
@@ -22,10 +24,16 @@ class Any implements SchemaItem, ElementItem, InterfaceSetMinMax
     protected ?string $namespace = null;
     protected ProcessContents $processContents = ProcessContents::Strict;
     protected ?string $id = null;
+    protected bool $local = false;
 
     public function __construct(Schema $schema)
     {
         $this->schema = $schema;
+    }
+
+    public function getType(): AnyType
+    {
+        return new AnyType($this->schema, null);
     }
 
     public function getName(): string
@@ -61,5 +69,15 @@ class Any implements SchemaItem, ElementItem, InterfaceSetMinMax
     public function setId(?string $id): void
     {
         $this->id = $id;
+    }
+
+    public function isLocal(): bool
+    {
+        return $this->local;
+    }
+
+    public function setLocal($local): void
+    {
+        $this->local = $local;
     }
 }
---
+++ src/Schema/Type/AnyType.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoetasWebservices\XML\XSDReader\Schema\Type;
+
+class AnyType extends Type
+{
+}
